95186e93e85de38e8c865da5506686dac87fd1ab
Isaac Heist <isaacheist@hotmail.com>
2020-12-29 12:16:18 -0800

    Initial set up of the project.

---
+++ Database.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C969___Scheduling_App___Isaac_Heist
+{
+    class Database
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["MySQLConnectionString"].ConnectionString;
+        private static MySqlConnection dbConnect = new MySqlConnection(connectionString);
+
+        public static List<User> getAllUsers()
+        {
+
+        }
+    }
+}
