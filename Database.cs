740d12c1cc5c82b2de6df600a072376fb3d812a8
Isaac Heist <isaacheist@hotmail.com>
2020-12-29 12:40:50 -0800

    Added getAllUsers static method to Database class.

--- Database.cs
+++ Database.cs
@@ -15,7 +15,30 @@ namespace C969___Scheduling_App___Isaac_Heist
 
         public static List<User> getAllUsers()
         {
+            List<User> listOfUsers = new List<User>();
+            string query = "SELECT * FROM user";
 
+            dbConnect.Open();
+            MySqlCommand cmd = new MySqlCommand(query, dbConnect);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                int userID = Convert.ToInt32(dataReader[0]);
+                string userName = dataReader[1].ToString();
+                string password = dataReader[2].ToString();
+                int active = Convert.ToInt32(dataReader[3]);
+                DateTime createDate = Convert.ToDateTime(dataReader[4]);
+                string createdBy = dataReader[5].ToString();
+                DateTime lastUpdate = Convert.ToDateTime(dataReader[6]);
+                string lastUpdateBy = dataReader[7].ToString();
+
+                listOfUsers.Add(new User(userID, userName, password, active, createDate, createdBy, lastUpdate, lastUpdateBy));
+            }
+
+            dbConnect.Close();
+
+            return listOfUsers;
         }
     }
 }
