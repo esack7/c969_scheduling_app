f9b240d2df19d129430d4d287c83586f8721c8ca
Isaac Heist <isaacheist@hotmail.com>
2020-12-29 16:57:45 -0800

    Finished Login Form including localization.

--- LogIn.Designer.cs
+++ LogIn.Designer.cs
@@ -29,7 +29,7 @@ namespace C969___Scheduling_App___Isaac_Heist
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.mainLabel = new System.Windows.Forms.Label();
             this.userNameTextBox = new System.Windows.Forms.TextBox();
             this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.userNameLabel = new System.Windows.Forms.Label();
@@ -38,20 +38,20 @@ namespace C969___Scheduling_App___Isaac_Heist
             this.errorLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // mainLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(179, 75);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(444, 32);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Customer Scheduling Application Log In";
+            this.mainLabel.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainLabel.Location = new System.Drawing.Point(0, 79);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(798, 32);
+            this.mainLabel.TabIndex = 0;
+            this.mainLabel.Text = "Customer Scheduling Application Log In";
+            this.mainLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // userNameTextBox
             // 
             this.userNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userNameTextBox.Location = new System.Drawing.Point(284, 179);
+            this.userNameTextBox.Location = new System.Drawing.Point(298, 179);
             this.userNameTextBox.Name = "userNameTextBox";
             this.userNameTextBox.Size = new System.Drawing.Size(205, 23);
             this.userNameTextBox.TabIndex = 1;
@@ -59,7 +59,7 @@ namespace C969___Scheduling_App___Isaac_Heist
             // passwordTextBox
             // 
             this.passwordTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordTextBox.Location = new System.Drawing.Point(284, 262);
+            this.passwordTextBox.Location = new System.Drawing.Point(298, 262);
             this.passwordTextBox.Name = "passwordTextBox";
             this.passwordTextBox.PasswordChar = '*';
             this.passwordTextBox.Size = new System.Drawing.Size(205, 23);
@@ -87,10 +87,11 @@ namespace C969___Scheduling_App___Isaac_Heist
             // 
             // loginButton
             // 
+            this.loginButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.loginButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginButton.Location = new System.Drawing.Point(284, 348);
+            this.loginButton.Location = new System.Drawing.Point(338, 365);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(102, 34);
+            this.loginButton.Size = new System.Drawing.Size(124, 44);
             this.loginButton.TabIndex = 5;
             this.loginButton.Text = "Log In";
             this.loginButton.UseVisualStyleBackColor = true;
@@ -101,7 +102,7 @@ namespace C969___Scheduling_App___Isaac_Heist
             this.errorLabel.AutoSize = true;
             this.errorLabel.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.errorLabel.ForeColor = System.Drawing.Color.Red;
-            this.errorLabel.Location = new System.Drawing.Point(281, 306);
+            this.errorLabel.Location = new System.Drawing.Point(295, 316);
             this.errorLabel.Name = "errorLabel";
             this.errorLabel.Size = new System.Drawing.Size(94, 13);
             this.errorLabel.TabIndex = 6;
@@ -118,7 +119,7 @@ namespace C969___Scheduling_App___Isaac_Heist
             this.Controls.Add(this.userNameLabel);
             this.Controls.Add(this.passwordTextBox);
             this.Controls.Add(this.userNameTextBox);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mainLabel);
             this.Name = "LogIn";
             this.Text = "Log In";
             this.Load += new System.EventHandler(this.LogIn_Load);
@@ -129,7 +130,7 @@ namespace C969___Scheduling_App___Isaac_Heist
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label mainLabel;
         private System.Windows.Forms.TextBox userNameTextBox;
         private System.Windows.Forms.TextBox passwordTextBox;
         private System.Windows.Forms.Label userNameLabel;
--- LogIn.cs
+++ LogIn.cs
@@ -1,27 +1,37 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace C969___Scheduling_App___Isaac_Heist
 {
     public partial class LogIn : Form
     {
-        List<User> users;
+        private List<User> users;
+        private string culture;
         public LogIn()
         {
             InitializeComponent();
         }
 
+        private void changeLoginToSpanish()
+        {
+            mainLabel.Text = "Iniciar sesión de Aplicación de Planificación de Clientes";
+            userNameLabel.Text = "Nombre de Usario";
+            passwordLabel.Text = "Contraseña";
+            loginButton.Text = "Iniciar Sesión";
+        }
         private void LogIn_Load(object sender, EventArgs e)
         {
+            culture = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
             users = Database.getAllUsers();
             errorLabel.Text = "";
+            if (culture == "es")
+            {
+                changeLoginToSpanish();
+            }
         }
 
         private void loginButton_Click(object sender, EventArgs e)
@@ -33,17 +43,31 @@ namespace C969___Scheduling_App___Isaac_Heist
             {
                 if(userName == "" || password == "")
                 {
+                    if (culture == "es")
+                    {
+                        throw new LoginException("Debe tener un nombre de usuario y una contraseña para iniciar sesión");
+                    }
+                    
                     throw new LoginException("You need to have both a User Name and Password to login");
                 }
-
-                var signedInUser = users.Where(user => user.UserName == userName).ToList();
+                //Below Lambda expession used to determine if user name entered match one of the user names returned from the database.
+                List<User> signedInUser = users.Where(user => user.UserName == userName).ToList();
+                
                 if (signedInUser.Count < 1)
                 {
+                    if (culture == "es")
+                    {
+                        throw new LoginException("El nombre de usuario que ingresó no existe.");
+                    }
                     throw new LoginException("The User Name you entered does not exist.");
                 }
 
                 if (signedInUser[0].Password != password)
                 {
+                    if (culture == "es")
+                    {
+                        throw new LoginException("La contraseña es incorrecta");
+                    }
                     throw new LoginException("The password is incorrect");
                 }
 
