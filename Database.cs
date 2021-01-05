ee3a1c722400ef801c66bf9af9c946d5b4dc5e06
Isaac Heist <isaacheist@hotmail.com>
2021-01-05 11:39:05 -0800

    Changes app to convert DateTimes saved in DB as UTC and DateTimes in program to be Local Time.

--- Database.cs
+++ Database.cs
@@ -29,9 +29,9 @@ namespace C969___Scheduling_App___Isaac_Heist
                 string userName = dataReader[1].ToString();
                 string password = dataReader[2].ToString();
                 int active = Convert.ToInt32(dataReader[3]);
-                DateTime createDate = Convert.ToDateTime(dataReader[4]);
+                DateTime createDate = Convert.ToDateTime(dataReader[4]).ToLocalTime();
                 string createdBy = dataReader[5].ToString();
-                DateTime lastUpdate = Convert.ToDateTime(dataReader[6]);
+                DateTime lastUpdate = Convert.ToDateTime(dataReader[6]).ToLocalTime();
                 string lastUpdateBy = dataReader[7].ToString();
 
                 listOfUsers.Add(new User(userID, userName, password, active, createDate, createdBy, lastUpdate, lastUpdateBy));
@@ -56,11 +56,11 @@ namespace C969___Scheduling_App___Isaac_Heist
                 int customerId = Convert.ToInt32(dataReader[1]);
                 int userId = Convert.ToInt32(dataReader[2]);
                 string type = dataReader[7].ToString();
-                DateTime start = Convert.ToDateTime(dataReader[9]);
-                DateTime end = Convert.ToDateTime(dataReader[10]);
-                DateTime createDate = Convert.ToDateTime(dataReader[11]);
+                DateTime start = Convert.ToDateTime(dataReader[9]).ToLocalTime();
+                DateTime end = Convert.ToDateTime(dataReader[10]).ToLocalTime();
+                DateTime createDate = Convert.ToDateTime(dataReader[11]).ToLocalTime();
                 string createdBy = dataReader[12].ToString();
-                DateTime lastUpdate = Convert.ToDateTime(dataReader[13]);
+                DateTime lastUpdate = Convert.ToDateTime(dataReader[13]).ToLocalTime();
                 string lastUpdateBy = dataReader[14].ToString();
 
                 MainScreen.ListOfAppointments.Add(new Appointment(appointmentId, customerId, userId, type, start, end, createDate, createdBy, lastUpdate, lastUpdateBy));
@@ -82,9 +82,9 @@ namespace C969___Scheduling_App___Isaac_Heist
                 string customerName = dataReader[1].ToString();
                 int addressID = Convert.ToInt32(dataReader[2]);
                 int active = Convert.ToInt32(dataReader[3]);
-                DateTime createDate = Convert.ToDateTime(dataReader[4]);
+                DateTime createDate = Convert.ToDateTime(dataReader[4]).ToLocalTime();
                 string createdBy = dataReader[5].ToString();
-                DateTime lastUpdate = Convert.ToDateTime(dataReader[6]);
+                DateTime lastUpdate = Convert.ToDateTime(dataReader[6]).ToLocalTime();
                 string lastUpdateBy = dataReader[7].ToString();
 
                 MainScreen.ListOfCustomers.Add(new Customer(customerID, customerName, addressID, active, createDate, createdBy, lastUpdate, lastUpdateBy));
@@ -99,7 +99,7 @@ namespace C969___Scheduling_App___Isaac_Heist
             var addedCustomer = new Customer(customerName, addressID, 1, now, user, now, user);
 
             dbConnect.Open();
-            string query = $"INSERT INTO `customer` VALUES ({addedCustomer.CustomerId}, '{addedCustomer.CustomerName}', {addedCustomer.AddressId}, {addedCustomer.Active}, '{addedCustomer.CreateDate.ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo)}', '{addedCustomer.CreatedBy}', '{addedCustomer.LastUpdate.ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo)}', '{addedCustomer.LastUpdateBy}')";
+            string query = $"INSERT INTO `customer` VALUES ({addedCustomer.CustomerId}, '{addedCustomer.CustomerName}', {addedCustomer.AddressId}, {addedCustomer.Active}, '{addedCustomer.CreateDate.ToUniversalTime().ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo)}', '{addedCustomer.CreatedBy}', '{addedCustomer.LastUpdate.ToUniversalTime().ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo)}', '{addedCustomer.LastUpdateBy}')";
             MySqlCommand cmd = new MySqlCommand(query, dbConnect);
             cmd.ExecuteNonQuery();
             dbConnect.Close();
@@ -122,7 +122,7 @@ namespace C969___Scheduling_App___Isaac_Heist
         public static void updateCustomer(Customer customer, string customerName, string user)
         {
             DateTime now = DateTime.Now;
-            string nowString = now.ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            string nowString = now.ToUniversalTime().ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
             dbConnect.Open();
             string query = $"UPDATE customer SET customerName='{customerName}', lastUpdate='{nowString}', lastUpdateBy='{user}' WHERE customerId={customer.CustomerId};";
             MySqlCommand cmd = new MySqlCommand(query, dbConnect);
@@ -152,9 +152,9 @@ namespace C969___Scheduling_App___Isaac_Heist
                 int cityID = Convert.ToInt32(dataReader[3]);
                 string postalCode = dataReader[4].ToString();
                 string phone = dataReader[5].ToString();
-                DateTime createDate = Convert.ToDateTime(dataReader[6]);
+                DateTime createDate = Convert.ToDateTime(dataReader[6]).ToLocalTime();
                 string createdBy = dataReader[7].ToString();
-                DateTime lastUpdate = Convert.ToDateTime(dataReader[8]);
+                DateTime lastUpdate = Convert.ToDateTime(dataReader[8]).ToLocalTime();
                 string lastUpdateBy = dataReader[9].ToString();
 
                 MainScreen.AddressDictionary.Add(addressID, new Address(addressID, address1, address2, cityID, postalCode, phone, createDate, createdBy, lastUpdate, lastUpdateBy));
@@ -168,7 +168,7 @@ namespace C969___Scheduling_App___Isaac_Heist
             var addedAddress = new Address(address1, address2, cityId, postalCode, phone, now, userName, now, userName);
 
             dbConnect.Open();
-            string query = $"INSERT INTO `address` VALUES ({addedAddress.AddressId}, '{addedAddress.AddressLine}', '{addedAddress.AddressLine2}', {addedAddress.CityId}, '{addedAddress.PostalCode}', '{addedAddress.Phone}', '{addedAddress.CreateDate.ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo)}', '{addedAddress.CreatedBy}', '{addedAddress.LastUpdate.ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo)}', '{addedAddress.LastUpdateBy}')";
+            string query = $"INSERT INTO `address` VALUES ({addedAddress.AddressId}, '{addedAddress.AddressLine}', '{addedAddress.AddressLine2}', {addedAddress.CityId}, '{addedAddress.PostalCode}', '{addedAddress.Phone}', '{addedAddress.CreateDate.ToUniversalTime().ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo)}', '{addedAddress.CreatedBy}', '{addedAddress.LastUpdate.ToUniversalTime().ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo)}', '{addedAddress.LastUpdateBy}')";
             MySqlCommand cmd = new MySqlCommand(query, dbConnect);
             cmd.ExecuteNonQuery();
             dbConnect.Close();
@@ -190,7 +190,7 @@ namespace C969___Scheduling_App___Isaac_Heist
         public static void updateAddress(Address address, string address1, string address2, int cityId, string postalCode, string phone, string user)
         {
             DateTime now = DateTime.Now;
-            string nowString = now.ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+            string nowString = now.ToUniversalTime().ToString("yy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
             dbConnect.Open();
             string query = $"UPDATE address SET address='{address1}', address2='{address2}', cityId={cityId}, postalCode='{postalCode}', phone='{phone}', lastUpdate='{nowString}', lastUpdateBy='{user}' WHERE addressId={address.AddressId};";
             MySqlCommand cmd = new MySqlCommand(query, dbConnect);
@@ -213,9 +213,9 @@ namespace C969___Scheduling_App___Isaac_Heist
                 int cityID = Convert.ToInt32(dataReader[0]);
                 string city = dataReader[1].ToString();
                 int countryID = Convert.ToInt32(dataReader[2]);
-                DateTime createDate = Convert.ToDateTime(dataReader[3]);
+                DateTime createDate = Convert.ToDateTime(dataReader[3]).ToLocalTime();
                 string createdBy = dataReader[4].ToString();
-                DateTime lastUpdate = Convert.ToDateTime(dataReader[5]);
+                DateTime lastUpdate = Convert.ToDateTime(dataReader[5]).ToLocalTime();
                 string lastUpdateBy = dataReader[6].ToString();
 
                 MainScreen.CityDictionary.Add(cityID, new City(cityID, city, countryID, createDate, createdBy, lastUpdate, lastUpdateBy));
@@ -235,9 +235,9 @@ namespace C969___Scheduling_App___Isaac_Heist
             {
                 int countryID = Convert.ToInt32(dataReader[0]);
                 string country = dataReader[1].ToString();
-                DateTime createDate = Convert.ToDateTime(dataReader[2]);
+                DateTime createDate = Convert.ToDateTime(dataReader[2]).ToLocalTime();
                 string createdBy = dataReader[3].ToString();
-                DateTime lastUpdate = Convert.ToDateTime(dataReader[4]);
+                DateTime lastUpdate = Convert.ToDateTime(dataReader[4]).ToLocalTime();
                 string lastUpdateBy = dataReader[5].ToString();
 
                 MainScreen.CountryDictionary.Add(countryID, new Country(countryID, country, createDate, createdBy, lastUpdate, lastUpdateBy));
