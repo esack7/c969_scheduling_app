88001a1716229193202321a2ed8bfd5e31d331a1
Isaac Heist <isaacheist@hotmail.com>
2021-01-05 20:26:59 -0800

    Finished functionality of changing calendar view.

--- AppointmentsForm.cs
+++ AppointmentsForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,12 +14,27 @@ namespace C969___Scheduling_App___Isaac_Heist
     public partial class AppointmentsForm : Form
     {
         private Form Main;
+        private DateTime SelectedDate;
+        private bool monthSelected = true;
+
         public AppointmentsForm(Form main)
         {
             InitializeComponent();
-            appointmentDataGridView.DataSource = MainScreen.ListOfAppointments;
             mainLabel.Text = $"Appointments for { MainScreen.LoggedInUser.UserName }";
             Main = main;
+            SelectedDate = DateTime.Now;
+        }
+
+        public void UpdateSelection()
+        {
+            if(monthSelected)
+            {
+                updateViewWithMonthlySelected();
+            } 
+            else
+            {
+                updateViewWithWeeklySelected();
+            }
         }
 
         private void backButton_Click(object sender, EventArgs e)
@@ -109,6 +125,89 @@ namespace C969___Scheduling_App___Isaac_Heist
             {
                 MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK);
             }
+        }
+
+        private void AppointmentsForm_Load(object sender, EventArgs e)
+        {
+            updateViewWithMonthlySelected();
+        }
+
+        private DateTime findBeginningOfWeek(DateTime date)
+        {
+            var culture = Thread.CurrentThread.CurrentCulture;
+            var difference = date.DayOfWeek - culture.DateTimeFormat.FirstDayOfWeek;
+            if(difference < 0)
+            {
+                difference = difference + 7;
+            }
+            return date.AddDays(-difference).Date;
+        }
+
+        private DateTime findEndOfWeek(DateTime date)
+        {
+            return findBeginningOfWeek(date).AddDays(7).AddMilliseconds(-1);
+        }
+
+        private DateTime findBeginningOfMonth(DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1);
+        }
+
+        private DateTime findEndOfMonth(DateTime date)
+        {
+            return findBeginningOfMonth(date).AddMonths(1).AddMilliseconds(-1);
+        }
+
+        private BindingList<Appointment> getAppointmentsInTimePeriod(DateTime beginTime, DateTime endTime)
+        {
+            //used the following lambda in linq statement to recreate list of Appointments that fall within the begin and end time bounds.
+            return new BindingList<Appointment>(MainScreen.ListOfAppointments.Where(appt => appt.Start >= beginTime && appt.End <= endTime).ToList());
+        }
+
+        private void monthRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if(monthRadioButton.Checked == true)
+            {
+                monthSelected = true;
+                updateViewWithMonthlySelected();
+            }
+        }
+
+        private void weekRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if(weekRadioButton.Checked == true)
+            {
+                monthSelected = false;
+                updateViewWithWeeklySelected();
+            }
+        }
+
+        private void monthCalendar_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            SelectedDate = monthCalendar.SelectionStart;
+
+            if(monthRadioButton.Checked == true)
+            {
+                updateViewWithMonthlySelected();
+            }
+            else
+            {
+                updateViewWithWeeklySelected();
+            }
+        }
+
+        private void updateViewWithMonthlySelected()
+        {
+            DateTime beginningOfMonth = findBeginningOfMonth(SelectedDate);
+            DateTime endOfMonth = findEndOfMonth(SelectedDate);
+            appointmentDataGridView.DataSource = getAppointmentsInTimePeriod(beginningOfMonth, endOfMonth);
+        }
+
+        private void updateViewWithWeeklySelected()
+        {
+            DateTime beginningOfWeek = findBeginningOfWeek(SelectedDate);
+            DateTime endOfWeek = findEndOfWeek(SelectedDate);
+            appointmentDataGridView.DataSource = getAppointmentsInTimePeriod(beginningOfWeek, endOfWeek);
         }
     }
 }
